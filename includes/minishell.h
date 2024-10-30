73ff619c554092e74e268b9a0db923c943518686
DeveloperDaniell <daniel.jelacik@gmail.com>
2024-10-30 16:52:25 +0200

    minor fixis

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,18 +6,17 @@
 /*   By: djelacik <djelacik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 15:44:43 by djelacik          #+#    #+#             */
-/*   Updated: 2024/10/30 15:44:44 by djelacik         ###   ########.fr       */
+/*   Updated: 2024/10/30 16:06:11 by djelacik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-
-# include <stdio.h> //printf
+# include <stdio.h> // printf
 # include <stdlib.h> // malloc
 
-typedef	struct s_tokens
+typedef struct s_tokens
 {
 	char	*token_string;
 	int		token_type;
