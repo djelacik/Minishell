613f6f89b0e64e650735bb39b946f74e7ceb0ac1
Mirella Jaakkola <mirellajaakkola@Mirellas-MacBook-Air.local>
2024-11-01 21:47:44 +0200

    added handling_quotes and hangling_env_var -m

--- src/parsing.c
+++ src/parsing.c
@@ -1,25 +1,134 @@
 #include "../includes/minishell.h"
 
-t_tokens	*tokenize_input(char *input)
+static int	count_tokens(char *input)
 {
-	char	**tokens_array;
-	t_tokens	*tokens;
+	char	quote_type;
+	char	*env_var;
+	int	count;
 	int	i;
 
-	tokens_array = ft_split(input, ' ');
-	tokens = malloc(10 * sizeof(t_tokens));
+	count = 0;
+	i = 0;
+	while (input[i])
+	{
+		while (input[i] == ' ')
+			i++;
+		if (!input[i])
+			break ;
+		if (input[i] == '\'' || input[i] == '"')
+		{
+			quote_type = input[i];
+			i++;
+			while (input[i] && input[i] != quote_type)
+			{
+				if (quote_type == '"' && input[i] == '$')
+				{
+					env_var = environment_variable(input, &i);
+					if (env_var)
+					{
+						count += ft_strlen(env_var);
+						free(env_var);
+					}
+				}
+				else
+				{
+					count++;
+					i++;
+				}
+			}
+			if (input[i] == quote_type)
+				i++;
+		}
+		else
+		{
+			while (input[i] && input[i] != ' ' && input[i] != '\'' && input[i] != '"')
+			{
+				if (input[i] == '$')
+				{
+					env_var = environment_variable(input, &i);
+					if (env_var)
+					{
+						count += ft_strlen(env_var);
+						free(env_var);
+					}
+				}
+				else
+				{
+					count++;
+					i++;
+				}
+			}
+		}
+		count++;
+	}
+	return (count);
+}
+
+static char *ft_strndup(const char *src, size_t n)
+{
+	char	*dst;
+	size_t	i;
+
+	dst = malloc((n + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+t_tokens	*tokenize_input(char *input)
+{
+	t_tokens	*tokens;
+	int		token_count;
+	int		i;
+	int		j;
+	int		start;
+
+	token_count = count_tokens(input);
+	tokens = malloc((token_count + 1) * sizeof(t_tokens));
 	if (!tokens)
 		return (NULL);
 	i = 0;
-	while (tokens_array[i] && i < 10)
+	j = 0;
+	while (input[i])
 	{
-		tokens[i].token_string = tokens_array[i];
-		if (i == 0)
-			tokens[i].token_type = COMMAND;
+		while (input[i] == ' ')
+			i++;
+		if (!input[i])
+			break;
+		if (input[i] == '\'' || input[i] == '"')
+			tokens[j].token_string = handling_quotes(input, &i);
+		else if (input[i] == '$')
+			tokens[j].token_string = environment_variable(input, &i);
 		else
-			tokens[i].token_type = ARGUMENT;
-		i++;
+		{
+			start = i;
+			while (input[i] && input[i] != ' ' && input[i] != '\'' && input[i] != '"')
+				i++;
+			tokens[j].token_string = ft_strndup(&input[start], i - start);
+		}
+		if (!tokens[j].token_string)
+		{
+			while (j > 0)
+			{
+				j--;
+				free(tokens[j].token_string);
+			}
+			free(tokens);
+			return (NULL);
+		}
+		if (j == 0)
+			tokens[j].token_type = COMMAND;
+		else
+			tokens[j].token_type = ARGUMENT;
+		j++;
 	}
-	free(tokens_array);
+	tokens[j].token_string = NULL;
 	return (tokens);
 }
