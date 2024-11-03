46c2ec95f242070d47df05bf0c73ca3bc7a10022
DeveloperDaniell <daniel.jelacik@gmail.com>
2024-11-03 12:21:31 +0200

    exec pwd added

--- includes/execution.h
+++ includes/execution.h
@@ -6,13 +6,14 @@
 /*   By: djelacik <djelacik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 19:25:53 by djelacik          #+#    #+#             */
-/*   Updated: 2024/11/02 19:27:26 by djelacik         ###   ########.fr       */
+/*   Updated: 2024/11/03 12:20:18 by djelacik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTION_H
 # define EXECUTION_H
 
-int		execute_echo(char **args);
+int		exec_echo(char **args);
+int		exec_pwd();
 
 #endif
