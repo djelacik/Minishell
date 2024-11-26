394570c4759aafa21eacc7efc9be9b98012c57ef
Mirella Jaakkola <mjaakkol@c1r6p14.hive.fi>
2024-11-26 13:38:09 +0200

    signals added -m

---
+++ src/signals.c
@@ -0,0 +1,27 @@
+#include "../includes/minishell.h"
+
+void	handle_sigint(int sig) // ctrl-c
+{
+	if (sig == SIGINT)
+	{
+		//rl_replace_line("", 0);
+		rl_on_new_line();
+		write(STDOUT_FILENO, "\n", 1);
+		rl_replace_line("", 0);
+		//rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+void	handle_sigquit(int sig)
+{
+	if (sig == SIGQUIT)
+	{
+		//write(STDOUT_FILENO, "", 0);
+		rl_replace_line("", 0);
+		write(STDOUT_FILENO, "", 0);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
