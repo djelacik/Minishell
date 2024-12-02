176ace7890829670df36caf27deb9567a8ac4351
Mirella Jaakkola <mirellajaakkola@Mirellas-MacBook-Air.local>
2024-12-02 14:43:12 +0200

    redir and append error fixed -m

--- src/handling_redir.c
+++ src/handling_redir.c
@@ -54,7 +54,7 @@ static char	*handle_here_doc(char *input, int *index, t_tokens *tokens)
 		tokens->token_type = REDIR_HERE_DOC;
 		symbol = ft_strndup("<<", 2);
 	}
-	(*index)++;
+	(*index) += 2;
 	return (symbol);
 }
 
@@ -92,7 +92,7 @@ static char	*handle_append_redir(char *input, int *index, t_tokens *tokens)
 	char	*symbol;
 	int		k;
 
-	k = (*index) + 1;
+	k = (*index) + 2;
 	while (input[k] == ' ')
 		k++;
 	if (input[k] == '\0')
@@ -110,19 +110,19 @@ static char	*handle_append_redir(char *input, int *index, t_tokens *tokens)
 	else
 	{
 		tokens->token_type = REDIR_APPEND;
-		symbol = ft_strndup(">>", 1);
+		symbol = ft_strndup(">>", 2);
 	}
-	(*index)++;
+	(*index) += 2;
 	return (symbol);
 }
 
 char	*redir_symb(char *input, int *index, t_tokens *tokens)
 {
-	if (input[*index] == '<')
+	if (input[*index] == '<' && input[*index + 1] != '<')
 		return (handle_input_redir(input, index, tokens));
 	else if (input[*index] == '<' && input[*index + 1] == '<')
 		return (handle_here_doc(input, index, tokens));
-	else if (input[*index] == '>')
+	else if (input[*index] == '>' && input[*index + 1] != '>')
 		return (handle_output_redir(input, index, tokens));
 	else if (input[*index] == '>' && input[*index + 1] == '>')
 		return (handle_append_redir(input, index, tokens));
