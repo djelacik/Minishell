46c98ed45813e204f8fa83ef0e2873e798e6dda9
Daniel Jelacik <143644426+djelacik@users.noreply.github.com>
2024-12-03 15:50:23 +0200

    append fixed

--- src/rest/debug.c
+++ src/rest/debug.c
@@ -6,10 +6,10 @@ void print_tokens(t_tokens *tokens, int token_count)
 
     while (tokens[i].token_string && i < token_count)
     {
-        dbg_print("  Token[%d]:\n", i);
-        dbg_print("    Token String: %s\n", tokens[i].token_string);
-        dbg_print("    Token Type: %d\n", tokens[i].token_type);
-        dbg_print("    Builtin Type: %d\n", tokens[i].builtin_type);
+        mprint("  Token[%d]:\n", i);
+        mprint("    Token String: %s\n", tokens[i].token_string);
+        mprint("    Token Type: %d\n", tokens[i].token_type);
+        mprint("    Builtin Type: %d\n", tokens[i].builtin_type);
         i++;
     }
 }
@@ -21,9 +21,9 @@ void print_redirects(t_redirect *redirs, int redir_count)
 
     while (i < redir_count)
     {
-        dbg_print("  Redirect[%d]:\n", i);
-        dbg_print("    File: %s\n", redirs[i].file);
-        dbg_print("    Type: %d\n", redirs[i].type);
+        mprint("  Redirect[%d]:\n", i);
+        mprint("    File: %s\n", redirs[i].file);
+        mprint("    Type: %d\n", redirs[i].type);
         i++;
     }
 }
@@ -34,17 +34,17 @@ void print_data(t_data *data, int data_count)
 
     while (i < data_count)
     {
-        dbg_print("Command[%d]:\n", i);
-        dbg_print("  Token Count: %d\n", data[i].token_count);
-        dbg_print("  Redirect Count: %d\n", data[i].redir_count);
+        mprint("Command[%d]:\n", i);
+        mprint("  Token Count: %d\n", data[i].token_count);
+        mprint("  Redirect Count: %d\n", data[i].redir_count);
         if (data[i].args)
         {
-            dbg_print("  Tokens:\n");
+            mprint("  Tokens:\n");
             print_tokens(data[i].args, data[i].token_count);
         }
         if (data[i].redirs)
         {
-            dbg_print("  Redirects:\n");
+            mprint("  Redirects:\n");
             print_redirects(data[i].redirs, data[i].redir_count);
         }
         i++;
@@ -55,9 +55,9 @@ void print_cmnds(t_cmnds *cmnds)
 {
     //int i = 0;
 
-    dbg_print("Command Count: %d\n", cmnds->data->cmnd_count);
-    dbg_print("Pipe Count: %d\n", cmnds->pipe_count);
+    mprint("Command Count: %d\n", cmnds->data->cmnd_count);
+    mprint("Pipe Count: %d\n", cmnds->pipe_count);
 
-    dbg_print("Commands:\n");
+    mprint("Commands:\n");
     print_data(cmnds->data, cmnds->data->cmnd_count);
 }
