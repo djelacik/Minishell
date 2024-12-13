19e5cc211263a2a49072264bcde04b9cb9048600
Daniel Jelacik <143644426+djelacik@users.noreply.github.com>
2024-12-13 16:38:46 +0200

    rest

--- src/signals.c
+++ src/signals.c
@@ -7,11 +7,24 @@ void	handle_sigint(int sig)
 	if (sig == SIGINT)
 	{
 		g_exit_status = 130;
+		printf("Sigint??\n");
 		write(1, "\n", 1);
-		//rl_replace_line("", 0);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
 	}
 	else if (sig == SIGQUIT)
 		g_exit_status = 131;
+}
+
+void here_doc_sig(int sig)
+{
+	if (sig == SIGINT)
+	{
+		g_exit_status = 130;
+		//write(1, "\n", 1);
+		printf("Control C??\n");
+		rl_replace_line("", 0);
+		exit(130);
+	}
 }
